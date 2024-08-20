24bfe71ca6e49952fff75e94932dfe6bf3b99f34
samit-java-developer <samitverma279@gmail.com>
2024-08-20 22:51:57 +0530

    added code to remove the duplicate from the linkedlist in java

--- src/com/samit/linkedlist/SinglyLinkedListExample.java
+++ src/com/samit/linkedlist/SinglyLinkedListExample.java
@@ -1,7 +1,9 @@
 package com.samit.linkedlist;
 
-public class SinglyLinkedList {
+public class SinglyLinkedListExample {
+	
 	private ListNode<Integer> head;
+	
 	private static class ListNode<T> {
 		private T data;
 		private ListNode<T> next;
@@ -11,6 +13,7 @@ public class SinglyLinkedList {
 			this.next = null;
 		}
 	}
+	
 	private void insertAtBegining(int data) {
 		ListNode<Integer> node = new ListNode<Integer>(data);
 		if (head == null) {
@@ -136,50 +139,29 @@ public class SinglyLinkedList {
 	}
 	
 	public static void main(String[] args) {
-		SinglyLinkedList sll = new SinglyLinkedList();
+		SinglyLinkedListExample sll = new SinglyLinkedListExample();
 		sll.head = new ListNode<Integer>(10);
-		System.out.println("Before insert at begining....");
+		
+		ListNode<Integer> first=new ListNode<Integer>(10);
+		ListNode<Integer> second=new ListNode<Integer>(10);
+		ListNode<Integer> third=new ListNode<Integer>(11);
+		ListNode<Integer> fourth=new ListNode<Integer>(11);
+		ListNode<Integer> fifth=new ListNode<Integer>(12);
+		ListNode<Integer> sixth=new ListNode<Integer>(13);
+		ListNode<Integer> seven=new ListNode<Integer>(13);
+		ListNode<Integer> eight=new ListNode<Integer>(14);
+		
+		sll.head.next=first;
+		first.next=second;
+		second.next=third;
+		third.next=fourth;
+		fourth.next=fifth;
+		fifth.next=sixth;
+		sixth.next=seven;
+		seven.next=eight;
+		
 		sll.display();
-		sll.insertAtBegining(8);
-		System.out.println("After insert at Begining....");
+		sll.removeDuplicateFromTheSortedList();
 		sll.display();
-
-		System.out.println("Before insert at End....");
-		sll.display();
-		sll.insertAtEnd(12);
-		System.out.println("After insert at End....");
-		sll.display();
-
-		System.out.println("Before insert at Middle....");
-		sll.display();
-		sll.insertAtMid(2, 11);
-		System.out.println("After insert at Middle....");
-		sll.display();
-
-		/*
-		 * System.out.println("Delete the node from the Beginning"); sll.deleteFirst();
-		 * sll.display();
-		 * 
-		 * System.out.println("Delete the node from the Last"); sll.deleteLast();
-		 * sll.display();
-		 */
-
-		/*
-		 * int data = 2;
-		 * System.out.println("Delete the node from the given position at " + data);
-		 * sll.deleteAtMid(data); sll.display();
-		 */
-
-		try {
-			System.out.println("Find nth node from the last...");
-			ListNode<Integer> temp = sll.getNthNodeFromTheLast(1);
-			System.out.println("The value of nth node from the end is "+temp.data);
-		} catch (Exception e) {
-			System.out.println("\nThe value of n is wrong....\n");
-		}
-		System.out.println("Reverse the linked list");
-		sll.reverseLinkedList();
-		sll.display();
-
 	}
 }
