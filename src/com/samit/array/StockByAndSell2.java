19d4541813fc898b80e4ce8dc5c2bb3c847677c9
samit-java-developer <samitverma279@gmail.com>
2024-11-05 09:09:17 +0530

    stockbuyandsell problem

---
+++ src/com/samit/array/StockByAndSell2.java
@@ -0,0 +1,14 @@
+package com.samit.array;
+
+public class StockByAndSell2 {
+
+	static int maxProfit(int[] price, int start, int end) {
+	}
+
+	public static void main(String[] args) {
+		int[] arr= {1,5,3,8,12};
+		int profit=maxProfit(arr, 0, arr.length-1);
+		System.out.println(profit);
+	}
+
+}
