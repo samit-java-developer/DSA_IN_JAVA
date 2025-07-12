b7d5f05b3c3cdd9c2263c52005d66f047ecad539
samit-java-developer <samitverma279@gmail.com>
2025-07-12 14:27:16 +0530

    remove duplicate from sorted array

--- src/com/samit/array/RemoveDuplicate.java
+++ src/com/samit/array/RemoveDuplicate.java
@@ -17,13 +17,27 @@ public class RemoveDuplicate {
 			}
 			i = j - 1;
 		}
-		return result.stream().mapToInt(ele -> ele).filter(x -> x % 2 == 0).toArray();
+		//return result.stream().mapToInt(ele -> ele).filter(x -> x % 2 == 0).toArray();
+		return result.stream().mapToInt(ele -> ele).toArray();
+	}
+	static void removeDuplicate(int[] arr){
+		int temp=arr[0];
+		System.out.print(temp+" ");
+		for (int i=1;i<arr.length;i++){
+			if (arr[i]==temp){
+				continue;
+			}
+			temp=arr[i];
+			System.out.print(temp+" ");
+		}
 	}
 
 	public static void main(String[] args) {
 		int[] arr = { 10, 10, 22, 22, 33 };
 		int[] res = removeDuplicate(arr, 5);
+		System.out.println();
 		System.out.println(Arrays.toString(res));
+		removeDuplicate(arr);
 	}
 
 }
