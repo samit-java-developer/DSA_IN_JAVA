89b7e78c39f1aeaa3ce700451c9d8e4cd5000f8a
samit-java-developer <samitverma279@gmail.com>
2025-10-08 22:53:42 +0530

    commited code of ComparatorInterFace, PartitionQuickSort,prime-factors and search minimum

--- src/com/samit/searching/coding/SearchMinimum.java
+++ src/com/samit/searching/coding/SearchMinimum.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class SearchMinimum {
 
     public static void main(String[] args) {
-        String line="32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31";
+        String line="10 1";
+        //String line="32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31";
         String[] lineArr=line.split(" ");
         int[] arr=new int[lineArr.length];
         for (int i=0;i<arr.length;i++){
@@ -14,10 +15,10 @@ public class SearchMinimum {
         System.out.println(Arrays.toString(arr));
         int x=minNumber(arr,0,arr.length-1);
         System.out.println(x);
+        x=minNumberSecondWay(arr,0,arr.length-1);
+        System.out.println(x);
     }
-
-
-    static int minNumber(int arr[], int low, int high) {
+    static int minNumber(int[] arr, int low, int high) {
         while (low<high){
             if (arr[low]<=arr[high]){
                 return arr[low];
@@ -30,6 +31,22 @@ public class SearchMinimum {
             }
         }
         return arr[low];
+    }
+
+    static int minNumberSecondWay(int[] arr,int low,int high){
+        int n=arr.length;
+        while (low<=high){
+            int mid=low+(high-low)/2;
+            if ((mid==0 && arr[mid]<arr[mid+1]) || (mid==n-1 && arr[mid]<arr[mid-1]) ||(mid!=0 && mid!=n-1 && arr[mid]<arr[mid+1] && arr[mid]<arr[mid-1])){
+                return arr[mid];
+            }
+            if (arr[mid]>=arr[0]){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return low==n?arr[low%n]:arr[low];
     }
 
 }
