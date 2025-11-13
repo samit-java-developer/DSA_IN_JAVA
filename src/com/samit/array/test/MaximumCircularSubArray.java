7b7bff8690bc0f5ba1f6f1f0c1490f80ccc1e541
samit-java-developer <samitverma279@gmail.com>
2025-11-14 00:23:17 +0530

    written code of max circular sub array sum

--- src/com/samit/array/test/MaximumCircularSubArray.java
+++ src/com/samit/array/test/MaximumCircularSubArray.java
@@ -3,9 +3,10 @@ package com.samit.array.test;
 public class MaximumCircularSubArray {
 
 	public static void main(String[] args) {
-		int[] arr = { 5, -2, 3, 4 };
-		// maximumCircularSubArray(arr);
-		printCirculerSubArray(arr);
+		int[] arr = {-4, -2, -1, -2, -6};
+		//printCirculerSubArray(arr);
+		int x=circlarSubArray(arr);
+		System.out.println(x);
 	}
 
 	static void printCirculerSubArray(int[] arr) {
@@ -23,6 +24,26 @@ public class MaximumCircularSubArray {
 				System.out.println();
 			}
 			System.out.println();
+		}
+	}
+
+	static int circlarSubArray(int[] arr){
+		int curr_max_sum=arr[0];
+		int curr_min_sum=arr[0];
+		int max_sum=arr[0];
+		int min_sum=arr[0];
+		int total_sum=arr[0];
+		for (int i=1;i<arr.length;i++){
+			curr_max_sum=Math.max(curr_max_sum+arr[i],arr[i]);
+			curr_min_sum=Math.min(curr_min_sum+arr[i],arr[i]);
+			max_sum=Math.max(max_sum,curr_max_sum);
+			min_sum=Math.min(min_sum,curr_min_sum);
+			total_sum+=arr[i];
+		}
+		if (max_sum<0){
+			return max_sum;
+		}else{
+			return Math.max(max_sum,total_sum-min_sum);
 		}
 	}
 }
