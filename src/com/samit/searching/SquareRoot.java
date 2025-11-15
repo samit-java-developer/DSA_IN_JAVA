1031839c38e0aca35df69a1d11f3e2fe1c46f8cc
samit-java-developer <samitverma279@gmail.com>
2025-11-15 23:12:18 +0530

    written code of square root

--- src/com/samit/searching/SquareRoot.java
+++ src/com/samit/searching/SquareRoot.java
@@ -2,8 +2,30 @@ package com.samit.searching;
 
 public class SquareRoot {
     public static void main(String[] args) {
-        int res=squareRoot(111);
+        int res=squareRoot(5);
         System.out.println(res);
+        res=new SquareRoot.Solution().floorSqrt(5);
+        System.out.println(res);
+    }
+
+    static class Solution {
+        int floorSqrt(int n) {
+            int low=1;
+            int high=n;
+            int ans=-1;
+            while (low<=high){
+                int mid=low+(high-low)/2;
+                if (mid*mid==n){
+                    return mid;
+                }else if (mid*mid>n){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                    ans=mid;
+                }
+            }
+            return ans;
+        }
     }
 
     static int squareRoot(int n){
