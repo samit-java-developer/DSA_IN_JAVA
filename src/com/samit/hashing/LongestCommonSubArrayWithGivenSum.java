7ad1b5ff81775aa58bbff3034a56c3f9405c0202
samit-java-developer <samitverma279@gmail.com>
2025-12-30 00:49:57 +0530

    written efficient code of longest sub-array with equal no of one and zero in two sub array

--- src/com/samit/hashing/LongestCommonSubArrayWithGivenSum.java
+++ src/com/samit/hashing/LongestCommonSubArrayWithGivenSum.java
@@ -1,11 +1,17 @@
 package com.samit.hashing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestCommonSubArrayWithGivenSum {
 
     public static void main(String[] args) {
         int[] arr1={0,1,0,0,0,0};
         int[] arr2={1,0,1,0,0,1};
         int res=longestCommonSubArrNaive(arr1,arr2);
+        System.out.println(res);
+
+        res=longestCommonSubArrEfficient(arr1,arr2);
         System.out.println(res);
     }
 
@@ -20,6 +26,27 @@ public class LongestCommonSubArrayWithGivenSum {
                 if (sum1==sum2){
                     res=Math.max(res,j-i+1);
                 }
+            }
+        }
+        return res;
+    }
+
+    static int longestCommonSubArrEfficient(int[] arr1,int[] arr2){
+        int n=arr1.length;
+        int[] temp=new int[n];
+        for (int i=0;i<n;i++){
+            temp[i]=arr1[i]-arr2[i];
+        }
+        int pre_sum=0;
+        Map<Integer,Integer> map=new HashMap<>();
+        map.put(0,-1);
+        int res=0;
+        for (int i=0;i<n;i++){
+            pre_sum+=temp[i];
+            if (map.containsKey(pre_sum)){
+                res=Math.max(res,i-map.get(pre_sum));
+            }else{
+                map.put(pre_sum,i);
             }
         }
         return res;
