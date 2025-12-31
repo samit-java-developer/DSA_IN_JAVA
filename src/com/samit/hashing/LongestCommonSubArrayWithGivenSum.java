325b1b3425807ba1d2d15c5d9ff07ab26dc668c8
samit-java-developer <samitverma279@gmail.com>
2025-12-31 20:38:35 +0530

    written efficient code of longest sub-array with equal no of one and zero in two sub array

--- src/com/samit/hashing/LongestCommonSubArrayWithGivenSum.java
+++ src/com/samit/hashing/LongestCommonSubArrayWithGivenSum.java
@@ -1,5 +1,6 @@
 package com.samit.hashing;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,6 +14,34 @@ public class LongestCommonSubArrayWithGivenSum {
 
         res=longestCommonSubArrEfficient(arr1,arr2);
         System.out.println(res);
+        int[] arr={0,1,2,1,0,2,1,0};
+        sortBooleanArray(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sortBooleanArray(int[] arr){
+        int n=arr.length;
+        int i=0;
+        int j=0;
+        int k=n-1;
+        while (j<=k){
+            if (arr[j]==0){
+                swap(arr,i,j);
+                i++;
+                j++;
+            }else if (arr[j]==1){
+                j++;
+            }else{
+                swap(arr,j,k);
+                k--;
+            }
+        }
+    }
+
+    static void swap(int[] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
 
     static int longestCommonSubArrNaive(int[] arr1,int[] arr2){
@@ -33,16 +62,16 @@ public class LongestCommonSubArrayWithGivenSum {
 
     static int longestCommonSubArrEfficient(int[] arr1,int[] arr2){
         int n=arr1.length;
-        int[] temp=new int[n];
-        for (int i=0;i<n;i++){
-            temp[i]=arr1[i]-arr2[i];
-        }
+//        int[] temp=new int[n];
+//        for (int i=0;i<n;i++){
+//            temp[i]=arr1[i]-arr2[i];
+//        }
         int pre_sum=0;
         Map<Integer,Integer> map=new HashMap<>();
         map.put(0,-1);
         int res=0;
         for (int i=0;i<n;i++){
-            pre_sum+=temp[i];
+            pre_sum+=arr1[i]-arr2[i];
             if (map.containsKey(pre_sum)){
                 res=Math.max(res,i-map.get(pre_sum));
             }else{
