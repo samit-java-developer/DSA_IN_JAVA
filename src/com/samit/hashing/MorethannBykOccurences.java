9dfca9c63ccb06af18b751a38634def20766367d
samit-java-developer <samitverma279@gmail.com>
2026-01-04 14:02:39 +0530

    written code of More than n by k occurences in so many ways

--- src/com/samit/hashing/MorethannBykOccurences.java
+++ src/com/samit/hashing/MorethannBykOccurences.java
@@ -1,5 +1,6 @@
 package com.samit.hashing;
 
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -10,9 +11,18 @@ public class MorethannBykOccurences {
     public static void main(String[] args) {
         int[] arr={10,20,30,10,10,20,20};
         int k=3;
+        long start=System.currentTimeMillis();
         printMoreThanNByK(arr,k);
-        System.out.println();
+        long end=System.currentTimeMillis();
+        System.out.println(end-start);
+        start=System.currentTimeMillis();
         efficient(arr,k);
+        end=System.currentTimeMillis();
+        System.out.println(end-start);
+        start=System.currentTimeMillis();
+        printMoreEfficient(arr,k);
+        end=System.currentTimeMillis();
+        System.out.println(end-start);
     }
 
     static void printMoreThanNByK(int[] arr,int k){
@@ -41,6 +51,42 @@ public class MorethannBykOccurences {
         }
         for (int x:hashTable.keySet()){
             if (hashTable.get(x)>n/k){
+                System.out.print(x+" ");
+            }
+        }
+        System.out.println();
+        for (Map.Entry<Integer,Integer> entry:hashTable.entrySet()){
+            if (entry.getValue()>n/k){
+                System.out.print(entry.getKey()+" ");
+            }
+        }
+    }
+
+    static void printMoreEfficient(int[] arr,int k){
+        int n=arr.length;
+        Map<Integer,Integer> hashTable=new LinkedHashMap<>();
+        for (int x:arr){
+            if (hashTable.containsKey(x)){
+                hashTable.put(x, hashTable.get(x)+1);
+            } else if (hashTable.size()<k-1) {
+                hashTable.put(x,1);
+            }else{
+                for(int key:hashTable.keySet()){
+                    hashTable.put(key,hashTable.get(key)-1);
+                    if (hashTable.get(key)==0){
+                        hashTable.remove(key);
+                    }
+                }
+            }
+        }
+        for (int x:hashTable.keySet()){
+            int count=0;
+            for (int i=0;i<n;i++){
+                if (x==arr[i]){
+                    count++;
+                }
+            }
+            if (count>n/k){
                 System.out.print(x+" ");
             }
         }
