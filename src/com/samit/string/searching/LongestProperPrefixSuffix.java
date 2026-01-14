b35e107015144ef68c4e98fe5665794f9b0491b6
samit-java-developer <samitverma279@gmail.com>
2026-01-14 23:10:20 +0530

    efficient code of finding the longest proper prefix and suffix

--- src/com/samit/string/searching/LongestProperPrefixSuffix.java
+++ src/com/samit/string/searching/LongestProperPrefixSuffix.java
@@ -3,7 +3,7 @@ package com.samit.string.searching;
 import java.util.Arrays;
 
 public class LongestProperPrefixSuffix {
-
+    // O(n^3)
     static int longestProperPrefixSuffix(String input,int n){
         for (int len=n-1;len>0;len--){
             boolean flag=true;
@@ -19,13 +19,44 @@ public class LongestProperPrefixSuffix {
         }
         return 0;
     }
+
+    //O(n) efficient solution of filling lps
+    static void fillLps(String input,int[] lps){
+        int n=input.length();
+        int len=0;
+        lps[0]=0;
+        int i=1;
+        while (i<n){
+            if (input.charAt(len)==input.charAt(i)){
+                lps[i]=len+1;
+                len++;
+                i++;
+            }else{
+                if (len==0){
+                    lps[i]=0;
+                    i++;
+                }else{
+                    len=lps[len-1];
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        String input="abacabad";
+        String input="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
         int n=input.length();
         int[] lps=new int[n];
+        long start=System.currentTimeMillis();
         for (int i=0;i<input.length();i++){
             lps[i]=longestProperPrefixSuffix(input,i+1);
         }
+        long end=System.currentTimeMillis();
         System.out.println(Arrays.toString(lps));
+        System.out.println(end-start);
+        start=System.currentTimeMillis();
+        fillLps(input,lps);
+        end=System.currentTimeMillis();
+        System.out.println(Arrays.toString(lps));
+        System.out.println(end-start);
     }
 }
