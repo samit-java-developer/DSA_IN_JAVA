6318034c2445b4cb2108e461558ba5bbbda30e37
samit-java-developer <samitverma279@gmail.com>
2026-02-03 00:45:41 +0530

    updated code

--- src/com/samit/stack/PreviousGreaterElement.java
+++ src/com/samit/stack/PreviousGreaterElement.java
@@ -7,13 +7,32 @@ public class PreviousGreaterElement {
     public static void main(String[] args) {
         int[] arr={20,30,10,5,15};
         printPreviousGreater(arr);
+        System.out.println();
+        printPreviousSmaller(arr);
     }
     static void printPreviousGreater(int[] arr){
         ArrayDeque<Integer> stack=new ArrayDeque<>();
         stack.push(arr[0]);
         System.out.print(-1+" ");
         for (int i=1;i<arr.length;i++){
-            while (!stack.isEmpty() && stack.peek()<=arr[i]){
+            while (!stack.isEmpty() && arr[i]>=stack.peek()){
+                stack.pop();
+            }
+            if (stack.isEmpty()){
+                System.out.print(-1+" ");
+            }else {
+                System.out.print(stack.peek()+" ");
+            }
+            stack.push(arr[i]);
+        }
+    }
+
+    static void printPreviousSmaller(int[] arr){
+        ArrayDeque<Integer> stack=new ArrayDeque<>();
+        stack.push(arr[0]);
+        System.out.print(-1+" ");
+        for (int i=1;i<arr.length;i++){
+            while (!stack.isEmpty() && arr[i]<=stack.peek()){
                 stack.pop();
             }
             if (stack.isEmpty()){
